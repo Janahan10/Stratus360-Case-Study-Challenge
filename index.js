725b9b1103079f8cca4b41205448c249d0d49cdf
Janahan <janahanravi10@gmail.com>
2021-06-05 13:09:41 -0400

    basic routing for comic no. in url and no comic no. implemented

--- index.js
+++
@@ -1,9 +0,0 @@
-const request = require('request')
-var PORT = 7600
-
-request('http://xkcd.com/251/info.0.json', {json: true}, (err, res, body) => {
-    if (err) {
-        return console.log(err)
-    }
-    console.log(body)
-}).listen(process.env.PORT)
---
+++ server.js
@@ -0,0 +1,61 @@
+/*
+import { createServer } from 'http';
+import { readFileSync } from 'fs';
+var index = readFileSync('index.html');
+
+createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(index);
+}).listen(9615);
+*/
+/*
+const http = require('http')
+var url = require('url')
+const fs = require('fs')
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+    const queryObject = url.parse(req.url, true).path
+    console.log(queryObject)
+
+    res.writeHead(200, {'content-type': 'text/html'})
+    res.write('hi')
+    res.end()
+})
+server.listen(port)
+*/
+
+const express = require('express')
+const axios = require('axios')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', function(req, res) {
+    axios.get('http://xkcd.com/251/info.0.json')
+    .then(response => {
+    console.log(response.data)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+})
+
+app.get('/:comicNo', function(req, res) {
+    const comicId = req.params.comicNo
+    res.send(comicId)
+    
+    axios.get('http://xkcd.com/' + req.params.comicNo + '/info.0.json')
+    .then(response => {
+    console.log(response.data)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+})
+
+app.listen(port)
+console.log('running...')
+
+
+
+    
