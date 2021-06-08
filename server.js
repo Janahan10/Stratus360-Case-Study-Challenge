3d7b80cea8873391c02a226cba72c4ea8cffe4c0
Janahan <janahanravi10@gmail.com>
2021-06-08 18:03:25 -0400

    added feature to count number of times a comic is viewed

--- server.js
+++ server.js
@@ -1,62 +1,71 @@
-/*
-import { createServer } from 'http';
-import { readFileSync } from 'fs';
-var index = readFileSync('index.html');
-
-createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(index);
-}).listen(9615);
-*/
-/*
-const http = require('http')
-var url = require('url')
-const fs = require('fs')
-const port = process.env.PORT || 3000
-
-const server = http.createServer((req, res) => {
-    const queryObject = url.parse(req.url, true).path
-    console.log(queryObject)
-
-    res.writeHead(200, {'content-type': 'text/html'})
-    res.write('hi')
-    res.end()
-})
-server.listen(port)
-*/
-
 const express = require('express')
 const axios = require('axios')
 const app = express()
 const port = process.env.PORT || 3000
 
+var comicNumber = 251
+var visted = {}
+
 app.set('view engine', 'ejs')
 
 app.get('/', function(req, res) {
+    comicNumber = 251
     axios.get('http://xkcd.com/251/info.0.json')
     .then(response => {
-    console.log(response.data)
+        console.log(comicNumber)
+    if (251 in visted) {
+        visted[251] += 1
+    } else {
+        visted[251] = 1
+    }
+    response.data["num_times"] = visted[251]
+    console.log(response.data["num_times"])
+    res.render('pages/index', response.data)
+    // console.log(response.data)
     })
     .catch(error => {
-        console.log(error)
+        //console.log(error)
     })
 })
 
 app.get('/:comicNo', function(req, res) {
-    const comicId = req.params.comicNo
-    let body = ''
-    
-    axios.get('http://xkcd.com/' + req.params.comicNo + '/info.0.json')
+    comicId = req.params.comicNo
+    comicNumber = req.params.comicNo
+    axios.get('http://xkcd.com/' + comicId + '/info.0.json')
     .then(response => {
+        console.log(comicNumber)
+        if (comicId in visted) {
+            visted[comicNumber] += 1
+        } else {
+            visted[comicNumber] = 1
+        }
+    
+        response.data["num_times"] = visted[comicNumber]
+        console.log(response.data["num_times"])
+
+    console.log(comicNumber)
     res.render('pages/index', response.data)
-    console.log(response.data)
+    //console.log(response.data)
     })
     .catch(error => {
-        console.log(error)
+        //console.log(error)
     })
-
-    
 })
+
+app.get('/next', function(req, res) {
+    comicId = comicNumber + 1
+    axios.get('http://xkcd.com/' + comicId + '/info.0.json')
+    .then(response => {
+    console.log(comicNumber)
+    res.render('pages/index', response.data)
+    //console.log(response.data)
+    })
+    .catch(error => {
+        //console.log(error)
+    })
+})
+
+
 
 app.listen(port)
 console.log('running...')
